7904db76ee92ffc9f51d51c53f0f314b9d89df0b
Magistr-George <65452211+Magistr-George@users.noreply.github.com>
2022-01-24 23:14:50 +0200

    Trying to do better

---
+++ src/test1/Secondary.java
@@ -0,0 +1,15 @@
+package test1;
+
+import javax.swing.JLabel;
+
+public class Secondary extends JLabel {
+    public int index = 0;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
