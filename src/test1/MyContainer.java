1600c315ecd0ead22ad0034dcd100f6dec440782
Magistr-George <65452211+Magistr-George@users.noreply.github.com>
2022-01-24 23:13:51 +0200

    Trying to do better

--- src/test1/MyContainer.java
+++ src/test1/MyContainer.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Set;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -12,116 +13,137 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 
-
-
 public class MyContainer extends JPanel implements MouseListener {
 
-	public int panelsCount=16;
-	int highlightIndex=6;
-	JLabel[] panels=new JLabel[panelsCount];	
-	SwingTemplate st;
-	public MyContainer(int windowWidth, int windowHeight, SwingTemplate st) {
-		this.setSize(windowWidth, windowHeight);
-		this.st=st;
-		
-		this.setLayout(new GridLayout(4,4));
-		
+    private int lenght = 4, width = 5;
+    private int panelsCount = lenght * width;
+    int highlightIndex = 6;
+    JLabel[] panels = new JLabel[panelsCount];
+    SwingTemplate st;
 
-		Font labelFont = this.getFont();
-		Font myFont=new Font(labelFont.getName(), Font.PLAIN, 30);
+    public MyContainer(int windowWidth, int windowHeight, SwingTemplate st) {
+        this.setSize(windowWidth, windowHeight);
+        this.st = st;
+
+        this.setLayout(new GridLayout(lenght, width));
 
 
-
-		for (int i=0;i<panelsCount;i++) {
-			panels[i] =new JLabel();
-			panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
-			panels[i].setFont(myFont);
-			panels[i].setText(String.valueOf(i));
-			panels[i].setHorizontalAlignment(SwingConstants.CENTER);
-			this.add(panels[i]);
-			
-			if (i==highlightIndex) {
-				panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
-			}
-		}
-
-		for (int i=0;i<panelsCount;i++) {
-			panels[i].addMouseListener(this);
-		}
-
-		sendInterfaceData();
-
-	}
-
-	private void sendInterfaceData() {
-		for (int i=0;i<panelsCount;i++) {
-			if (i==highlightIndex) {
-				panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
-			}else {
-				panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
-			}
-		}
-		st.setTitle("Selected index is " + String.valueOf(highlightIndex));
-	}
-	
+        Font labelFont = this.getFont();
+        Font myFont = new Font(labelFont.getName(), Font.PLAIN, 30);
 
 
-	public void keyLeft() {
-		if (highlightIndex>0)
-			highlightIndex--;
-		
-		sendInterfaceData();
-	}
+        for (int i = 0; i < panelsCount; i++) {
+            panels[i] = new JLabel();
+            panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+            panels[i].setFont(myFont);
+            panels[i].setText(String.valueOf(i));
+            panels[i].setHorizontalAlignment(SwingConstants.CENTER);
+            this.add(panels[i]);
+            //panels[i].setIndex(i);
+
+            if (i == highlightIndex) {
+                panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
+            }
+        }
+
+        for (int i = 0; i < panelsCount; i++) {
+            panels[i].addMouseListener(this);
+        }
+
+        sendInterfaceData();
+
+    }
+
+    private void sendInterfaceData() {
+        for (int i = 0; i < panelsCount; i++) {
+            if (i == highlightIndex) {
+                panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
+            } else {
+                panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+            }
+        }
+        st.setTitle("Selected index is " + String.valueOf(highlightIndex));
+    }
 
 
-	public void keyRight() {
-		if (highlightIndex<panelsCount-1)
-			highlightIndex++;	
-			
-		sendInterfaceData();
-	}
+    public void keyLeft() {
+        if (highlightIndex > 0) {
+            highlightIndex--;
+        }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
+        sendInterfaceData();
+    }
 
-	}
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		for (int i=0;i<panelsCount;i++) {
-			if (panels[i]==e.getComponent()) {
-				if(panels[i].isVisible()) st.setTitle("Selected index is " + String.valueOf(panels[i].getText()));
+    public void keyRight() {
+        if (highlightIndex < panelsCount - 1) {
+            highlightIndex++;
+        }
 
-					while (i+4<panelsCount) {
-						if (panels[i+4].isVisible()) {
-							panels[i].setText(String.valueOf(panels[i+4].getText()));
-						}else break;
+        sendInterfaceData();
+    }
 
-						i+=4;
-					}
-				panels[i].setVisible(false);
-			}
-		}
-	}
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //Unused
+    }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
+    /*@Override
+    public void mousePressed(MouseEvent e) {
+        for (int i = 0; i < panelsCount; i++) {
+            if (panels[i] == e.getComponent()) {
+                if (panels[i].isVisible()) {
+                    st.setTitle("Selected index is " + String.valueOf(panels[i].getText()));
+                }
 
-	}
+                while (i + width < panelsCount) {
+                    if (panels[i + width].isVisible()) {
+                        panels[i].setText(String.valueOf(panels[i + width].getText()));
+                    } else break;
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		for (int i=0;i<panelsCount;i++) {
-			if (panels[i]==e.getComponent()) {
-				panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
-			}else {
-				panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
-			}
-		}
-	}
+                    i += width;
+                }
+                panels[i].setVisible(false);
+            }
+        }
+    }*/
 
-	@Override
-	public void mouseExited(MouseEvent e) {
+    public void mousePressed(MouseEvent e) {
 
-	}
+                if (e.getComponent().isVisible()) {
+                    st.setTitle("Selected index is " + String.valueOf(e.getComponent().getText()));
+                }
+
+                while (e.getComponent().getIndex() + width < panelsCount) {
+                    if (e.getComponent().setIndex(index + width).isVisible()) {
+                        e.getComponent.setText(String.valueOf(e.getComponent().setIndex(index + width).getText()));
+                    } else break;
+
+                    i += width;
+                }
+                e.getComponent.setVisible(false);
+            }
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //Unused
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        for (int i = 0; i < panelsCount; i++) {
+            if (panels[i] == e.getComponent()) {
+                panels[i].setBorder(BorderFactory.createLineBorder(Color.green, 3));
+            } else {
+                panels[i].setBorder(BorderFactory.createLineBorder(Color.blue));
+            }
+        }
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //Unused
+    }
 }
